913dc30aa07a41a673632fd703f3405ce269c410
Victor Lopes <135762344+floppinho@users.noreply.github.com>
2023-06-08 13:40:59 -0300

    Automatizar caixa de mercado

---
+++ Mercado.js
@@ -0,0 +1,16 @@
+const endereço = "Avenida 1";
+
+let preço_do_leite = 9.5;
+let preço_do_queijo = 30; 
+
+console.log ("Preço do leite:", preço_do_leite); 
+console.log ("Preço do queijo:", preço_do_queijo);
+
+preço_do_leite = 8;
+
+
+
+
+
+
+
